6de86908e33e7e9c151ee3787d3c71565fc31055
Lauren Renaud <lrenaud@andrew.cmu.edu>
2017-04-07 00:31:11 -0400

    working version that generates individual reports (but not consolidated yet)

--- template_consolidated.R
+++
@@ -1,3 +0,0 @@
-# need to create template for each line of the report, for each location essentiall
-# and then run a loop that will run for each location and then print all on same page, maybe rbind
-# or maybe this can be done in the run_reports.R script
