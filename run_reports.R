95cf7709de93ef120a52e2c7778f0c2baff00844
Lauren Renaud <lrenaud@andrew.cmu.edu>
2017-04-07 00:31:11 -0400

    working version that generates individual reports (but not consolidated yet)

--- run_reports.R
+++ run_reports.R
@@ -1,6 +1,7 @@
 library(rmarkdown)
 library(dplyr)
 library(lubridate)
+library(readr)
 #library(RPostgreSQL)
 #library(jsonlite)
 
@@ -24,46 +25,26 @@ library(lubridate)
 #property <- dbGetQuery(conn, "SELECT * FROM tblProperty")
 
 # pulling in CSVs ------
-field_data <- read.csv("MOCK_DATA.csv", header=TRUE, sep=",")
+field_data <- readr::read_csv("Codefest Data.csv")
 
-# data cleaning -----
-
-#### DATA INTERPRETATION PROBLEM
-## It appears that EventDate is the date a case was opened
-## And ResolvedDate is when it's closed
-## But there's missingness in the ResolvedDate
-## And also does not appear to have a date for
-## follow visits
-## dataframe below is for figuring out differnce between
-## ComplianceDate, DateResolved, and how they connect to EventDate
-# testingDates <- muni.data %>%
-#   select(EventID, EventDate, ComplianceDate, DateResolved, OverallStatus)
-
-# # close the connection
-# RPostgreSQL::postgresqlCloseConnection(conn)
-# 
-# # setup logging
-# flog.appender(appender.file("logs/make_reports.log"), name="log")
-# flog.threshold(DEBUG, name="log")
-
+recent_disaster <- unique(field_data$disaster_id[field_data$date == max(field_data$date)])
 
 # Generate reports -------
 
-generateReport <- function(disasterid, locationid, field_data){
+generateReportHTML <- function(disasterid, locationid, field_data){
   #' Generates a report and stores it as the location with .pdf
   #' appended in the /output directory for that disaster id.
   #' @param disasterid     code for particular disaster
   #' @param locationid     id for specific location
   #' @param field_data      data input from field via app
-  require(lubridate)
   require(rmarkdown)
   require(dplyr)
   
   # only make the report if it doesn't already exist
   if(!file.exists(paste("output/", disasterid, locationid, ".html", sep=""))){
-    rmarkdown::render("template_individual.rmd",
+    rmarkdown::render("template_individual_html.rmd",
                       #output_format = pdf_document,
-                      output_file = paste(disasterid, "_", locationid, ".html", sep=""),
+                      output_file = paste("Disaster", disasterid, "_ReportID", locationid, ".html", sep=""),
                       #output_file = paste(disasterid, "_", locationid, ".pdf", sep=""),
                       output_dir = paste("ReportOutput/", disasterid, sep=""),
                       runtime = "static",
@@ -79,6 +60,61 @@ generateReport <- function(disasterid, locationid, field_data){
 }
 
 
+generateReportPDF <- function(disasterid, locationid, field_data){
+  #' Generates a report and stores it as the location with .pdf
+  #' appended in the /output directory for that disaster id.
+  #' @param disasterid     code for particular disaster
+  #' @param locationid     id for specific location
+  #' @param field_data      data input from field via app
+  require(rmarkdown)
+  require(dplyr)
+  
+  # only make the report if it doesn't already exist
+  if(!file.exists(paste("output/", disasterid, locationid, ".pdf", sep=""))){
+    rmarkdown::render("template_individual_pdf.rmd",
+                      #output_format = pdf_document,
+                      output_file = paste("Disaster", disasterid, "_ReportID", locationid, ".pdf", sep=""),
+                      output_dir = paste("ReportOutput/", disasterid, sep=""),
+                      runtime = "static",
+                      envir = new.env(),
+                      intermediates_dir = "temp",
+                      params=list(
+                        disasterid = disasterid,
+                        locationid = locationid,
+                        field_data = field_data
+                      )
+    )
+  }
+}
+
+
+
+generateConsolidatedReportPDF <- function(disasterid, field_data){
+  #' Generates a report and stores it as the location with .pdf
+  #' appended in the /output directory for that disaster id.
+  #' @param disasterid     code for particular disaster
+  #' @param field_data      data input from field via app
+  require(rmarkdown)
+  require(dplyr)
+  
+  # only make the report if it doesn't already exist
+  if(!file.exists(paste("output/Consolidated_", disasterid, ".pdf", sep=""))){
+    rmarkdown::render("template_individual_pdf.rmd",
+                      #output_format = pdf_document,
+                      output_file = paste("Consolidated_Disaster", disasterid, ".pdf", sep=""),
+                      output_dir = paste("ReportOutput/", disasterid, sep=""),
+                      runtime = "static",
+                      envir = new.env(),
+                      intermediates_dir = "temp",
+                      params=list(
+                        disasterid = disasterid,
+                        field_data = field_data
+                      )
+    )
+  }
+}
+
+
 
 makeAllReports <- function(disasterid_selected, field_data){
   #' Calls generateReport for each location
@@ -87,10 +123,13 @@ makeAllReports <- function(disasterid_selected, field_data){
   #' @param field_data      data input from field via app
   
   for(id_selected in field_data$id[field_data$disaster_id == disasterid_selected]){
-    generateReport(disasterid = disasterid_selected, locationid = id_selected, field_data)
+    generateReportHTML(disasterid = disasterid_selected, locationid = id_selected, field_data)
+    generateReportPDF(disasterid = disasterid_selected, locationid = id_selected, field_data)
   }
 }
 
 # make all reports
-###### NEED TO FIGURE OUT HOW TO RUN FOR THIS DISASTER, NOT PRIOR?
-makeAllReports(disasterid, field_data)
+makeAllReports(recent_disaster, field_data)
+
+# make consolidated report
+
